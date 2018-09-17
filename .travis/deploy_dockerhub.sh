c532d2330f9395e6a3ee5e6cb3306027ebcb6a4e
pkolobaev <pkolobaev@amt.ru>
2018-09-17 19:42:17 +0300

    changed shell script for pushing to docker hub

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker login -u $DOCKER_USER -p $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="$TRAVIS_BRANCH"
     docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
