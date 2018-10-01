ee702b2383f28999e15f7c9ee9d52ee8f5db50c0
pkolobaev <pkolobaev@amt.ru>
2018-10-01 20:26:50 +0300

    1. Added application properties for deploying in Heroku 2, Changed dockerfile for security 3. Changed docker_start script

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM openjdk:8-jre-stretch
 MAINTAINER pkolobaev
 COPY target/stickers.jar /
+RUN useradd -m stickers
+USER stickers
 CMD java -jar stickers.jar
--- scripts/docker_start.cmd
+++ scripts/docker_start.cmd
@@ -2,4 +2,4 @@ docker ps --filter ancestor=stickers -a -q>stickers.txt
 for /F "tokens=*" %%A in (stickers.txt) do docker rm -f %%A
 del stickers.txt
 docker build -t stickers ..
-docker run -p 8080:8080 stickers
+docker run -p 9087:9087 -e PORT=9087 stickers
