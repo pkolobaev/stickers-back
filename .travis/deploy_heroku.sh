87e8c1dfad83b8aefd3c0eebdd076db12e505edb
pkolobaev <pkolobaev@amt.ru>
2018-10-05 18:42:13 +0300

    1. changed incorrect command in shel script

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -2,7 +2,7 @@
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh &&
     heroku plugins:install @heroku-cli/plugin-container-registry &&
-    docker login -p=$HEROKU_API_KEY registry.heroku.com &&
+    docker login -u=$HEROKU_EMAIL -p=$HEROKU_API_KEY registry.heroku.com &&
     heroku container:push web --app $HEROKU_APP_NAME &&
     heroku container:release web --app $HEROKU_APP_NAME
 fi
