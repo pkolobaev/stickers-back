d33de5daedcfe538c059fc8a730a95a1f8c11950
pkolobaev <pkolobaev@amt.ru>
2018-10-05 18:29:35 +0300

    1. Added shell script for deploying on heroku 2. Changed travis.yml

---
+++ .travis/deploy_heroku.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+    wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh &&
+    heroku plugins:install @heroku-cli/plugin-container-registry &&
+    docker login -e _ -u _ --password=$HEROKU_API_KEY registry.heroku.com &&
+    heroku container:push web --app $HEROKU_APP_NAME &&
+    heroku container:release web --app $HEROKU_APP_NAME
+fi
