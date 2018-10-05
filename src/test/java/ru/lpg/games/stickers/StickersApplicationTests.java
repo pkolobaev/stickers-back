c27824c39544863eb4abbbb0add3efe905ae8609
pkolobaev <pkolobaev@amt.ru>
2018-10-05 19:01:33 +0300

    checking pipeline one more time

--- src/test/java/ru/lpg/games/stickers/StickersApplicationTests.java
+++ src/test/java/ru/lpg/games/stickers/StickersApplicationTests.java
@@ -22,6 +22,6 @@ public class StickersApplicationTests {
                 .expectStatus()
                 .isOk()
                 .expectBody(String.class)
-                .isEqualTo("STICKERS1");
+                .isEqualTo("STICKERS");
     }
 }
