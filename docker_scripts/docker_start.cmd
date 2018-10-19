7083d7299da7aedee2da4243646e73d7178faf32
pkolobaev <pkolobaev@amt.ru>
2018-10-19 17:00:05 +0300

    SB-2: comments for script with startup into docker

--- docker_scripts/docker_start.cmd
+++ docker_scripts/docker_start.cmd
@@ -1,5 +1,10 @@
+:: Ищем запущенные контейнеры с приложением и записываем их id в файл (криво, надо будет поправить...)
 docker ps --filter ancestor=stickers -a -q>stickers.txt
+:: Убиваем все запущенные контейнеры
 for /F "tokens=*" %%A in (stickers.txt) do docker rm -f %%A
+:: Удаляем файл
 del stickers.txt
+:: Билдим образ контейнера из Dockerfile в родительской директории проекта
 docker build -t stickers ..
+:: Запускаем контейнер
 docker run -p 9087:9087 -e PORT=9087 stickers
