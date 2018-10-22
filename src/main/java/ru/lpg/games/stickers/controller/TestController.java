28e8799ec3de6e2a67f46d14b251a30c3f9ce507
pkolobaev <pkolobaev@amt.ru>
2018-10-22 16:26:38 +0300

    SB-4: changed media type of response.

--- src/main/java/ru/lpg/games/stickers/controller/TestController.java
+++ src/main/java/ru/lpg/games/stickers/controller/TestController.java
@@ -1,11 +1,13 @@
 package ru.lpg.games.stickers.controller;
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.json.JSONObject;
 
 
 @RestController
@@ -13,8 +15,8 @@ public class TestController {
 
     private final Logger log = LoggerFactory.getLogger(TestController.class);
 
-    @RequestMapping(value = "/stickers", method = RequestMethod.GET, produces = MediaType.TEXT_PLAIN_VALUE)
+    @RequestMapping(value = "/stickers", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public String test() {
-        return "STICKERS";
+        return new JSONObject().put("Greeting","Welcome to Stickers").toString();
     }
 }
